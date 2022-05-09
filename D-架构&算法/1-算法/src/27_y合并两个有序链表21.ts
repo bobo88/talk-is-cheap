68e93aa7463be1e44adb1b3f6a2228cf13734627
bobo88 <yuanboi88@163.com>
2022-05-09 15:43:06 +0800

    206. 反转链表

--- D-架构&算法/1-算法/src/27_y合并两个有序链表21.ts
+++ D-架构&算法/1-算法/src/27_y合并两个有序链表21.ts
@@ -34,7 +34,20 @@
         }
 
         // 方法二： 迭代（官方）
-        // todo
+        // const  prehead = new ListNode(-1);
+        // let prev = prehead;
+        // while(list1 !== null && list2 !== null) {
+        //     if (list1.val < list2.val) {
+        //         prev.next = list1;
+        //         list1 = list1.next;
+        //     } else {
+        //         prev.next = list2;
+        //         list2 = list2.next;
+        //     }
+        //     prev = prev.next;
+        // }
+        // prev.next = list1 === null ? list2 : list1;
+        // return prehead.next
     
     };
 
