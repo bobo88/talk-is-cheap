fe115c402797eb7120a5d60761c56eaf828b89cb
bobo88 <yuanboi88@163.com>
2022-07-18 18:10:17 +0800

    todo-598. 范围求和 II

--- D-架构&算法/1-算法/src/111_y范围求和II598.ts
+++ D-架构&算法/1-算法/src/111_y范围求和II598.ts
@@ -18,8 +18,13 @@
 
     function maxCount(m: number, n: number, ops: number[][]): number {
         // 方法一：
-        
-        return 1;
+        let mina = m, minb = n;
+        for (const op of ops) {
+            mina = Math.min(mina, op[0]);
+            minb = Math.min(minb, op[1]);
+        }
+        return mina * minb;
+
 
         // 方法二：
 
